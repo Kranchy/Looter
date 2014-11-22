20e84fa4c594a9c0e3a1ac1cfb6427207beb026e
Kong- <jeffcoeur@gmail.com>
2014-11-22 17:25:26 +0100

    Multiple animations

--- Project/Assets/Scripts/Managers/InputManager.cs
+++ Project/Assets/Scripts/Managers/InputManager.cs
@@ -3,111 +3,130 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour
 {
+    public Player Player;
+    public Rigidbody2D RigidBody;
+    public SpriteRenderer LowerRenderer;
+    public SpriteRenderer UpperRenderer;
+    public ItemMenu ItemMenu;
 
-		Player player;
-		Rigidbody2D rb;
-		int jumpTime;
-		Vector3 movement;
-		public ItemMenu im;
-		SpriteRenderer sr;
-		int currentAnim = 0;
+	int JumpTime;
+	Vector3 Movement;
+	int CurrentAnim = 0;
 
-
-
-		// Use this for initialization
-		void Start ()
-		{
-				player = gameObject.GetComponent ("Player") as Player;
-				rb = gameObject.GetComponent ("Rigidbody2D") as Rigidbody2D;
-				sr = gameObject.GetComponent ("SpriteRenderer") as SpriteRenderer;
-				sr.sprite = player.AnimationDroite [0];
-		}
+	// Use this for initialization
+	void Start ()
+	{
+        LowerRenderer.sprite = Player.LowerAnimRight[0];
+        UpperRenderer.sprite = Player.UpperAnimRight[0];
+    }
 	
-		// Update is called once per frame
-		void Update ()
-		{
+	// Update is called once per frame
+	void Update ()
+	{
+		// Capture des mouvements
 
-				// Capture des mouvements
+		if (Input.GetAxis ("Horizontal") < 0)
+        {
+			gameObject.transform.Translate (new Vector3 (-Player.Speed, 0, 0));
+			if (Player.OnGround)
+            {
+				if (CurrentAnim == Player.LowerAnimLeft.Count - 1)
+                {
+					CurrentAnim = 0;
+			    }
+                else
+                {
+		    		CurrentAnim ++;
+	    		}
+                LowerRenderer.sprite = Player.LowerAnimLeft[CurrentAnim];
+                UpperRenderer.sprite = Player.UpperAnimLeft[CurrentAnim];
 
-				if (Input.GetAxis ("Horizontal") < 0) {
-						gameObject.transform.Translate (new Vector3 (-player.Speed, 0, 0));
-						if (player.OnGround) {
-								if (currentAnim == player.AnimationGauche.Count - 1) {
-										currentAnim = 0;
-								} else {
-										currentAnim ++;
-								}
-								sr.sprite = player.AnimationGauche [currentAnim];
-
-						}
-			else{
-				if (currentAnim == player.AnimationSautGauche.Count - 1) {
-					currentAnim = 0;
-				} else {
-					currentAnim ++;
-				}
-				sr.sprite = player.AnimationSautGauche [currentAnim];
 			}
+		    else
+            {
+				if (CurrentAnim == Player.AnimationSautGauche.Count - 1)
+                {
+				    CurrentAnim = 0;
 				}
-				if (Input.GetAxis ("Horizontal") > 0) {
-						gameObject.transform.Translate (new Vector3 (player.Speed, 0, 0));
-						if (player.OnGround) {
-								if (currentAnim == player.AnimationDroite.Count - 1) {
-										currentAnim = 0;
-								} else {
-										currentAnim ++;
-								}
-								sr.sprite = player.AnimationDroite [currentAnim];
-						}
-			else{
-				if (currentAnim == player.AnimationSautDroite.Count - 1) {
-					currentAnim = 0;
-				} else {
-					currentAnim ++;
-				}
-				sr.sprite = player.AnimationSautDroite [currentAnim];
+                else
+                {
+					CurrentAnim ++;
+                }
+				LowerRenderer.sprite = Player.AnimationSautGauche[CurrentAnim];
 			}
-
+        }
+		if (Input.GetAxis ("Horizontal") > 0)
+        {
+            gameObject.transform.Translate (new Vector3 (Player.Speed, 0, 0));
+            if (Player.OnGround)
+            {
+                if (CurrentAnim == Player.LowerAnimRight.Count - 1)
+                {
+                    CurrentAnim = 0;
+                } 
+                else
+                {
+                    CurrentAnim ++;
+                }
+                LowerRenderer.sprite = Player.LowerAnimRight[CurrentAnim];
+                UpperRenderer.sprite = Player.UpperAnimRight[CurrentAnim];
+            }
+			else
+            {
+				if (CurrentAnim == Player.AnimationSautDroite.Count - 1) {
+					CurrentAnim = 0;
+				} else {
+					CurrentAnim ++;
 				}
-				if (Input.GetAxis ("Jump") > 0) {
-
-						if (player.Booted || player.OnGround) {
-
-								jumpTime = 5;
-						}
-				}
-				if (jumpTime > 0) {
-						rb.AddForce (Vector3.up * 100);
-						jumpTime -= 1;
-				}
-
-				//Capture des actions
-		
-				if (Input.GetAxis ("Weapon") > 0) {
-						if (Input.GetAxis ("ItemMenu") > 0) {
-								im.OpenWeaponMenu ();
-						} else {
-
-						}
-				}
-
-				if (Input.GetAxis ("Util") > 0) {
-						if (Input.GetAxis ("ItemMenu") > 0) {
-								im.OpenUtilMenu ();
-						} else {
-				
-						}
-				}
-
-				if (Input.GetAxis ("Passive") > 0) {
-						if (Input.GetAxis ("ItemMenu") > 0) {
-								im.OpenPassiveMenu ();
-						} else {
-				
-						}
-				}
-
-
-
-		}
+				LowerRenderer.sprite = Player.AnimationSautDroite [CurrentAnim];
+			}
+        }
+        if (Input.GetAxis ("Jump") > 0)
+        {
+            if (Player.Booted || Player.OnGround)
+            {
+                JumpTime = 5;
+            }
+        }
+        if (JumpTime > 0)
+        {
+            RigidBody.AddForce (Vector3.up * 100);
+            JumpTime -= 1;
+        }
+        
+        //Capture des actions
+        
+        if (Input.GetAxis ("Weapon") > 0)
+        {
+            if (Input.GetAxis ("ItemMenu") > 0)
+            {
+                ItemMenu.OpenWeaponMenu ();
+            }
+            else
+            {
+            }
+        }
+        
+        if (Input.GetAxis ("Util") > 0)
+        {
+            if (Input.GetAxis ("ItemMenu") > 0)
+            {
+                ItemMenu.OpenUtilMenu ();
+            }
+            else
+            {
+            }
+        }
+        
+        if (Input.GetAxis ("Passive") > 0)
+        {
+            if (Input.GetAxis ("ItemMenu") > 0)
+            {
+                ItemMenu.OpenPassiveMenu ();
+            }
+            else
+            {
+            }
+        }
+    }
 }
