a8355982bcf6f4c33815f7fbb620ee265c8145de
Kranchy <yannickvautier@hotmail.fr>
2014-11-23 02:49:01 +0100

    Repassage release

---
+++ Project/Assets/Scripts/Humans/Human.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour {
+
+	public int HP;
+	public float Speed;
+	public float JumpSpeed;
+}
---
+++ Project/Assets/Scripts/Items/Passives/Boots.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boots : Passive {
+
+	// Use this for initialization
+    public override string Name
+    {
+        get { return "Boots"; }
+    }
+
+    public override string Description
+    {
+        get { return "Do a double jump!"; }
+    }
+
+    void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public override void OnEquip(Player player)
+    {
+        player.Booted = true;
+    }
+
+    public override void OnUnequip(Player player)
+    {
+        player.Booted = false;
+    }
+}
