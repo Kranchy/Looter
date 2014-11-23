61642ae453274411c88b774c3dbe124bdde3b7f5
SigoriV <legrandlucas@orange.fr>
2014-11-23 02:55:49 +0100

    Ajoute animation pose bombe ET animation rework

---
+++ Project/Assets/Scripts/Humans/Enemy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Human {
+
+	Weapon Weapon;
+	public Object AI;
+
+}
---
+++ Project/Assets/Scripts/Items/Inherited/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Item : MonoBehaviour
+{
+    public abstract string Name { get; }
+
+    public abstract string Description { get; }
+
+    public Texture2D Icon;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+}
