564f83abf23f411003733d175e66b7389838a4cb
Kranchy <yannickvautier@hotmail.fr>
2014-11-23 10:43:07 +0100

    health

---
+++ Project/Assets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((gameObject.GetComponent ("Human") as Human).HP == 0) {
+			Destroy (gameObject);
+				}
+
+		if (gameObject.transform.position.y < -10) {
+
+			(gameObject.GetComponent ("Human") as Human).HP = 0;
+			Application.LoadLevel (Application.loadedLevel);
+
+				}
+	}
+}
