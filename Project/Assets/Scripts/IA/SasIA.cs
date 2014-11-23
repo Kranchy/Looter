bfcb30f3a5996acacda85ce4a4c41a549fde0349
Kranchy <yannickvautier@hotmail.fr>
2014-11-23 11:47:39 +0100

    SAS menu

---
+++ Project/Assets/Scripts/IA/SasIA.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SasIA : MonoBehaviour {
+
+	public Player player;
+	public ItemMenu itemMenu;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((player.transform.position.x >= transform.position.x - 1) && (player.transform.position.x <= transform.position.x + 1)) {
+						if ((player.transform.position.y >= transform.position.y - 3) && (player.transform.position.y <= transform.position.y + 3)) {
+								itemMenu.OpenSasMenu ();
+						}
+			else {
+				itemMenu.CloseSasMenu ();
+			}
+
+
+				} 
+	}
+}
